ab0cbbce18f4cf9cbe9d2cc651d7fe82038e2ae3
Jakub Sukiennik <kuba1207710@gmail.com>
2024-03-24 18:10:02 +0100

    add tests

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -1,18 +1,27 @@
-describe('Swiper Gallery Test', function () {
+describe("Swiper Gallery Test", function () {
   it('Checks if second slide contains "United Kingdom"', function () {
-    cy.visit('http://localhost:3000');
-    cy.get('.swiper-button-next').click();
-    cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
+    cy.visit("http://localhost:3000");
+    cy.get(".swiper-button-next").click();
+    cy.get(".swiper-slide-active").should("contain", "United Kingdom");
   });
-});
 
-describe('Swiper Gallery Test', function () {
+  it("Properly swipes back and forth", function () {
+    cy.visit("http://localhost:3000");
+    cy.get(".swiper-slide-active").should("contain", "Italy");
+    cy.get(".swiper-button-next").click();
+    cy.get(".swiper-slide-active").should("contain", "United Kingdom");
+    cy.wait(2000);
+    cy.get(".swiper-button-prev").click();
+    cy.wait(2000);
+    cy.get(".swiper-slide-active").should("contain", "Italy");
+  });
+
   it('Checks if third slide contains "Paris"', function () {
-    cy.visit('http://localhost:3000');
-    cy.get('.swiper-button-next').click();
+    cy.visit("http://localhost:3000");
+    cy.get(".swiper-button-next").click();
     cy.wait(2000);
-    cy.get('.swiper-button-next').click({ force: true });
+    cy.get(".swiper-button-next").click({ force: true });
     cy.wait(2000);
-    cy.get('.swiper-slide-active').should('contain', 'Paris');
+    cy.get(".swiper-slide-active").should("contain", "Paris");
   });
 });
